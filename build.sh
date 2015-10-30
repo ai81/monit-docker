cc872c5ef435ebfbd9049a4509a6f5e288306f71
Anton Ivanov <antonio@yandex-team.ru>
2015-10-30 19:08:21 +0300

    prepare to use gmail + little changes

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 MHOST=$(hostname -s) docker-compose up -d && docker tag monitdocker_monit ai81/monit-docker:latest
--- build/start.sh
+++ build/start.sh
@@ -7,12 +7,13 @@ sed -i "s/MAIL_SERVER/$MAIL_SERVER/g" /etc/monit/conf.d/common.conf
 sed -i "s/ALERT_EMAIL/$ALERT_EMAIL/g" /etc/monit/conf.d/common.conf
 sed -i "s/MONIT_USER/$MONIT_USER/g" /etc/monit/conf.d/common.conf
 sed -i "s/MONIT_PASS/$MONIT_PASS/g" /etc/monit/conf.d/common.conf
+sed -i "s/PASSWORD/$PASSWORD/g" /etc/monit/conf.d/common.conf
 
 ls /hostsrv/ | grep -v moint | xargs -I {} ln -s /hostsrv/{} /srv/{}
 
 monit -I &
 sleep 3
-tail -F /var/log/monit &
+tail -F /var/log/monit.log &
 
 echo "start watcher for /etc/monit/conf.d/"
 while inotifywait -r -e modify /etc/monit/conf.d/ ; do /srv/monit/monit.sh; done
